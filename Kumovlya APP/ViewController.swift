bc9dc71a9a31826ab1a6f0bf537aa36090714c7f
alexslk <alexdneprin@icloud.com>
2023-04-25 21:04:26 +0100

    project created

---
+++ Kumovlya APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kumovlya APP
+//
+//  Created by Slobodianiuk Oleksandr on 25.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
